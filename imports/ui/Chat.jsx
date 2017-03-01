e494598199ec0484e92bb8a688724770f30834e1
Kyle <kyle@linuxbox.hsd1.il.comcast.net>
2017-02-28 20:32:10 -0600

    add transition to chat bubble

--- imports/ui/Chat.jsx
+++ imports/ui/Chat.jsx
@@ -1,21 +1,54 @@
 import React, { Component, PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import { Meteor } from 'meteor/meteor';
 
 export default class Chat extends Component {
+	constructor(props) {
+		super(props);
+		const audio = new Audio('https://www.freesound.org/people/HerbertBoland/sounds/33369/download/33369__herbertboland__mouthpop.wav');
+		this.state = {
+			popSound: audio,
+			playSound: true,
+		}
+	}
 
 	componentDidUpdate() {
 		const chatbox = document.getElementById("chat-jumbo");
 		chatbox.scrollTop = chatbox.scrollTopMax;
+		//check to see if sound can play
+		if (this.state.playSound) {
+			//set the sound to not play again until timeout reached
+			this.setState({ playSound: false })
+			//play the pop sound
+			this.state.popSound.play();
+			//rate limit the pop sound to one per second
+			setTimeout(function() {
+				this.setState({ playSound: true })
+			}, 1000);
+		}
+			
 	}
+
+	
 
 	render() {
 
-		const createDate= new Date(this.props.chat.createdAt);
-		const hours = createDate.getHours();
-		const min = createDate.getMinutes();
-		const formatMin = (min < 10 ? '0':'') + min;
-		const timeStamp = hours + ':' + formatMin;
+		const createDate = new Date(this.props.chat.createdAt);
+		const checkDate = new Date(this.props.chat.createdAt);
+		let timeStamp;
+		if (checkDate.setHours(0,0,0,0) == new Date().setHours(0,0,0,0)) {
+			const hours = createDate.getHours();
+			const min = createDate.getMinutes();
+			const formatMin = (min < 10 ? '0':'') + min;
+			timeStamp = hours + ':' + formatMin;
+		} else {
+			const day = createDate.getDate();
+			const month = createDate.getMonth();
+			const year = createDate.getFullYear();
+			timeStamp = month + "/" + day + "/" + year;
+		}
+
 		const author = this.props.chat.author;
 		const user = Meteor.users.findOne({_id: author});
 		const userImage = user.services.google.picture;
@@ -24,22 +57,36 @@ export default class Chat extends Component {
 		
 
 		return (
-			<div className="row">
-				<div className="col-sm-10">
-					<p className="chat-bubble">{this.props.chat.text}</p>
-				</div>
-				<div className="col-sm-2">
-					<span className="author-name float-sm-left">
-						<img
-							src={userImage}
-							className="chat-image" />
-						{userName}
-					</span>
-					<span className="author-date float-sm-right">
-						{timeStamp}
-					</span>
-
-				</div>
+			<div>
+				<ReactCSSTransitionGroup
+		      transitionName="chat-animation"
+		      transitionAppear={true}
+		      transitionAppearTimeout={500}
+		      transitionEnter={false}
+		      transitionLeave={false}>
+					<div className="row">
+						<div className="col-sm-2">
+							<span className="author-name">
+								<img
+									src={userImage}
+									className="chat-image" />
+								{userName}
+							</span>
+						</div>
+					</div>
+					<div className="row chat-bubble-row">
+						<div className="col-sm-10">
+							<p className="chat-bubble">
+								{this.props.chat.text}
+							</p>
+						</div>
+						<div className="col-sm-2">
+							<p className="author-date float-sm-right">
+								{timeStamp}
+							</p>
+						</div>
+					</div>
+				</ReactCSSTransitionGroup >
 			</div>
 
 		);
